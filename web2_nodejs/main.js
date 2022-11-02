a7b4bc81c23d5bc27dfefffa66043e697bcf62ba
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2022-11-03 05:20:12 +0900

    CRUD - C

--- web2_nodejs/main.js
+++ web2_nodejs/main.js
@@ -86,12 +86,17 @@ var app = http.createServer(function (request, response) {
         });
         request.on('end', function () {
             var post = qs.parse(body);
-            var title = post.title;
-            var description = post.description
-            fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
-                response.writeHead(302, { Location: `/?id=${title}` });
-                response.end();
-            });
+            db.query(`
+            INSERT INTO topic (title, description, created, author_id) 
+            VALUES(?, ?, NOW(), ?)`,
+                [post.title, post.description, 1],
+                function (error, result) {
+                    if (error) {
+                        throw error;
+                    }
+                    response.writeHead(302, { Location: `/?id=${result.insertId}` });
+                    response.end();
+                });
         });
     } else if (pathname === '/update') {
         fs.readdir('./data', function (error, filelist) {
