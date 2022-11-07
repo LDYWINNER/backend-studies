c8178f077f19e51eac76c65b9b45e69128e84f10
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2022-11-08 01:54:39 +0900

    using static files + dealing with 404 errors

--- express/main.js
+++ express/main.js
@@ -150,24 +150,28 @@ var fs = require('fs');
 var template = require('./lib/template.js');
 var path = require('path');
 var sanitizeHtml = require('sanitize-html');
-var qs = require('querystring');
 var bodyParser = require('body-parser');
 var compression = require('compression');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(compression());
+app.use(express.static('public'));
 app.get('*', function (request, response, next) {
     fs.readdir('./data', function (error, filelist) {
         request.list = filelist;
         next();
     });
 });
+
 app.get('/', function (request, response) {
     var title = 'Welcome';
     var description = 'Hello, Node.js';
     var list = template.list(request.list);
     var html = template.HTML(title, list,
-        `<h2>${title}</h2>${description}`,
+        `
+        <h2>${title}</h2>${description}
+        <img src="/images/dog.jpeg" style="width:300px; display: block; margin-top: 10px;">
+        `,
         `<a href="/create">create</a>`
     );
     response.send(html);
@@ -175,22 +179,26 @@ app.get('/', function (request, response) {
 app.get('/page/:pageId', function (request, response) {
     var filteredId = path.parse(request.params.pageId).base;
     fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
-        var title = request.params.pageId;
-        var sanitizedTitle = sanitizeHtml(title);
-        var sanitizedDescription = sanitizeHtml(description, {
-            allowedTags: ['h1']
-        });
-        var list = template.list(request.list);
-        var html = template.HTML(sanitizedTitle, list,
-            `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-            ` <a href="/create">create</a>
+        if (err) {
+            next(err);
+        } else {
+            var title = request.params.pageId;
+            var sanitizedTitle = sanitizeHtml(title);
+            var sanitizedDescription = sanitizeHtml(description, {
+                allowedTags: ['h1']
+            });
+            var list = template.list(request.list);
+            var html = template.HTML(sanitizedTitle, list,
+                `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
+                ` <a href="/create">create</a>
                 <a href="/update/${sanitizedTitle}">update</a>
                 <form action="/delete_process" method="post">
                     <input type="hidden" name="id" value="${sanitizedTitle}">
                     <input type="submit" value="delete">
                 </form>`
-        );
-        response.send(html);
+            );
+            response.send(html);
+        }
     });
 });
 app.get('/create', function (request, response) {
@@ -263,6 +271,15 @@ app.post('/delete_process', function (request, response) {
     });
 });
 
+app.use(function (req, res, next) {
+    res.status(404).send('Sorry cant find that!');
+});
+
+app.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).send('Something broke!!');
+});
+
 app.listen(3003, function () {
-    console.log('Example app listening on port 3000!')
+    console.log('Example app listening on port 3000!');
 });
