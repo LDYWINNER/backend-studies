338e66449993d598f0cc4ed293674425c7f7029d
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2022-11-08 15:08:42 +0900

    using helmet module

--- express/main.js
+++ express/main.js
@@ -149,31 +149,33 @@ var app = express();
 var fs = require("fs");
 var bodyParser = require("body-parser");
 var compression = require("compression");
+var helmet = require('helmet');
 var indexRouter = require("./routes/index");
 var topicRouter = require("./routes/topic");
 
+app.use(helmet());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(compression());
 app.use(express.static("public"));
 app.get("*", function (request, response, next) {
-  fs.readdir("./data", function (error, filelist) {
-    request.list = filelist;
-    next();
-  });
+    fs.readdir("./data", function (error, filelist) {
+        request.list = filelist;
+        next();
+    });
 });
 
 app.use("/", indexRouter);
 app.use("/topic", topicRouter);
 
 app.use(function (req, res, next) {
-  res.status(404).send("Sorry cant find that!");
+    res.status(404).send("Sorry cant find that!");
 });
 
 app.use(function (err, req, res, next) {
-  console.error(err.stack);
-  res.status(500).send("Something broke!!");
+    console.error(err.stack);
+    res.status(500).send("Something broke!!");
 });
 
 app.listen(3003, function () {
-  console.log("Example app listening on port 3000!");
+    console.log("Example app listening on port 3000!");
 });
