1410c7ad0d1078e3795719c64344f4ce086c3489
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2024-04-01 02:39:29 -0400

    SETUP - starting graphql server

--- tweetql/server.js
+++ tweetql/server.js
@@ -1,5 +1,14 @@
 import { ApolloServer, gql } from "apollo-server";
 
+const typeDefs = gql`
+  type Query {
+    text: String
+    hello: String
+  }
+`;
+
+const server = new ApolloServer({ typeDefs });
+
 server.listen().then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
 });
