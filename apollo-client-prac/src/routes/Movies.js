cb6be69ac9b2f39f49f5fc3f435a147d53e29f95
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2024-04-02 15:04:55 -0400

    useQuery

--- apollo-client-prac/src/routes/Movies.js
+++ apollo-client-prac/src/routes/Movies.js
@@ -1,9 +1,41 @@
-function Movies() {
+import { gql, useQuery } from "@apollo/client";
+
+const ALL_MOVIES = gql`
+  query getMovies {
+    allMovies {
+      title
+      id
+    }
+    allTweets {
+      id
+      text
+      author {
+        fullName
+      }
+    }
+  }
+`;
+
+export default function Movies() {
+  const { data, loading, error } = useQuery(ALL_MOVIES);
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
+  if (error) {
+    return <h1>Could not fetch :(</h1>;
+  }
   return (
-    <div>
+    <ul>
       <h1>Movies</h1>
-    </div>
+      {data.allMovies.map((movie) => (
+        <li key={movie.id}>{movie.title}</li>
+      ))}
+      <h1>Tweets</h1>
+      {data.allTweets.map((tweet) => (
+        <li key={tweet.id}>
+          {tweet.text}/by: {tweet.author.fullName}
+        </li>
+      ))}
+    </ul>
   );
 }
-
-export default Movies;
