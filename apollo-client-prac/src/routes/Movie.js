8938cf7567a16e4b1fe5b96652cdfefbd0b55f59
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2024-04-02 15:26:08 -0400

    useQuery with var

--- apollo-client-prac/src/routes/Movie.js
+++ apollo-client-prac/src/routes/Movie.js
@@ -1,9 +1,25 @@
-function Movie() {
-  return (
-    <div>
-      <h1>Movie</h1>
-    </div>
-  );
-}
+import { gql, useQuery } from "@apollo/client";
+import { useParams } from "react-router-dom";
 
-export default Movie;
+const GET_MOVIE = gql`
+  query getMovie($movieId: String!) {
+    movie(id: $movieId) {
+      id
+      title
+    }
+  }
+`;
+
+export default function Movie() {
+  const { id } = useParams();
+  const { data, loading } = useQuery(GET_MOVIE, {
+    variables: {
+      movieId: id,
+    },
+  });
+  console.log(data, loading);
+  if (loading) {
+    return <h1>Fetching movie...</h1>;
+  }
+  return <div>{data.movie.title}</div>;
+}
