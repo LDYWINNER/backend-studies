e981c38123791ad6a4f5ad79a8d46f96f4a66ee9
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2024-04-16 01:12:28 -0400

    SETUP - nextjs 14 prac

--- nextjs14-prac/app/about-us/page.tsx
+++ nextjs14-prac/app/about-us/page.tsx
@@ -1,7 +1,13 @@
-export default function Tomato() {
+export const metadata = {
+  title: "About us",
+};
+
+export default function AboutUs() {
   return (
     <div>
       <h1>About us!</h1>
     </div>
   );
 }
+
+export const runtime = "edge";
